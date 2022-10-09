d781a22a89af6a29dfea96a3e9e40c69016a2988
Caleb Leinz <cleinz+github@pm.me>
2022-10-09 13:17:53 -0700

    Added configuration for interview preparation

---
+++ .config/nvim_interview/lua/settings/init.lua
@@ -0,0 +1,25 @@
+local set = vim.opt
+
+set.expandtab = true
+set.smarttab = true
+set.hlsearch = true
+
+set.shiftwidth = 4
+set.tabstop = 4
+set.ignorecase = true
+set.smartcase = true
+
+set.splitbelow = true
+set.splitright = true
+set.number = true
+set.relativenumber = true
+set.autoindent = true
+set.scrolloff = 5
+set.fileencoding = 'utf-8'
+
+set.wrap = false
+
+-- COLORSCHEME
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme peachpuff]])
+vim.g.airline_theme = "base16_gruvbox_dark_medium"
