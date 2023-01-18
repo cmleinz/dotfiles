c9831d1a24c7c2758b6b3dfea03090faa3b4f81b
Caleb Leinz <cleinz+github@pm.me>
2023-01-17 19:56:45 -0800

    Added wezterm configuration

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require 'wezterm'
+
+return {
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
+  font = wezterm.font 'ComicCodeLigatures',
+  color_scheme = "Gruvbox dark, medium (base16)",
+}
+
