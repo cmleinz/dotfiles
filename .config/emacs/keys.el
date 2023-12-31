bce6168f9f4ca383d531fced75855a30fe6a9c82
Caleb <cleinz+github@pm.me>
2023-12-31 15:32:06 -0800

    Add keys.el

---
+++ .config/emacs/keys.el
@@ -0,0 +1,111 @@
+(use-package general
+  :config
+  (general-evil-setup t)
+  (general-override-mode)
+  (general-define-key
+   :states '(normal visual insert emacs)
+   :keymaps 'override
+   :prefix "SPC"
+   :non-normal-prefix "C-SPC"
+    "o"  '(:ignore t :which-key "Org")
+    "x"  '(execute-extended-command :which-key "M-x")
+
+    "b"  '(:ignore t :which-key "Buffers")
+    "bb" '(counsel-switch-buffer :which-key "Switch buffer")
+    "bn" '(evil-next-buffer :which-key "Next buffer")
+    "bN" '(evil-prev-buffer :which-key "Previous buffer")
+    "bk" '(kill-buffer :which-key "Kill buffer")
+
+    "f"  '(:ignore t :which-key "Files")
+    "fd" '(dired-jump :which-key "Open file folder")
+    "fs" '(save-buffer :which-key "save-buffer")
+    "ff" '(counsel-find-file :which-key "counsel-find-file")
+    "fr" '(counsel-recentf :which-key "counsel-recentf")
+
+    "w"  '(:ignore t :which-key "Windows")
+    "ws" '(evil-window-new :which-key "Split window horizontally")
+    "wv" '(evil-window-vnew :which-key "Split window vertically")
+    "wd" '(evil-window-delete :which-key "Delete window")
+    "wo" '(delete-other-windows :which-key "Delete other window")
+    "wj" '(evil-window-down :which-key "Switch to window down")
+    "wk" '(evil-window-up :which-key "Switch to window up")
+    "wh" '(evil-window-left :which-key "Switch to window left")
+    "wl" '(evil-window-right :which-key "Switch to window right")
+    "wJ" '(evil-window-move-very-bottom :which-key "Move window down")
+    "wK" '(evil-window-move-very-top :which-key "Move window up")
+    "wH" '(evil-window-move-far-left :which-key "Move window left")
+    "wL" '(evil-window-move-far-right :which-key "Move window right")
+
+    ;; LSP Mode bindings
+    "c"  '(:ignore t :which-key "Code")
+    "cg" '(:ignore t :which-key "goto")
+    "cgd" '(lsp-find-definition :which-key "Find Definition")
+    "cgr" '(lsp-find-references :which-key "Find References")
+    "cgn" '(lsp-ui-find-next-reference :which-key "Find Next Reference")
+    "cgN" '(lsp-ui-find-prev-reference :which-key "Find Previous Reference")
+    "cgi" '(lsp-find-implementation :which-key "Find Implementation")
+    "cgc" '(lsp-find-declaration :which-key "Find Declaration")
+    "ci" '(counsel-imenu :which-key "Counsel Imenu")
+    "cs" '(lsp-ui-find-workspace-symbol :which-key "Find Workspace Symbol")
+    "cf" '(lsp-ui-peek-find-definitions :which-key "Show function definition")
+    "ce" '(flycheck-list-errors :which-key "List Errors")
+    "ck" '(lsp-ui-doc-toggle :which-key "Show item docs")
+    "cp" '(check-parens :which-key "Check parenthesis errors")
+    "ca" '(lsp-execute-code-action :which-key "Perform code actions") 
+    "cr" '(lsp-rename :which-key "Rename")
+
+    "p"  '(:ignore t :which-key "Project")
+    "pr" '(projectile-run-project :which-key "Run Project")
+    "pc" '(projectile-compile-project :which-key "Compile Project")
+    "pf" '(counsel-projectile-find-file :which-key "Find File")
+    "pd" '(counsel-projectile-find-dir :which-key "Find Dir")
+    "pp" '(counsel-projectile-switch-project :which-key "Switch Project")
+    "pb" '(counsel-projectile-switch-to-buffer :which-key "Switch Buffer")
+    "po" '(counsel-projectile-org-capture :which-key "Org Capture")
+    "pg" '(counsel-projectile-rg :which-key "Ripgrep")
+
+    "h"  '(:ignore t :which-key "Helper")
+    "ht" '(counsel-load-theme :which-key "Load theme")
+
+    "g"  '(:ignore t :which-key "Magit")
+    "gg" '(magit-status :which-key "magit-status")
+    "gf" '(magit-fetch :which-key "magit-fetch") 
+    "gF" '(magit-fetch-all :which-key "magit-fetch-all") 
+    "gb" '(magit-branch :which-key "magit-branch")
+    "gp" '(magit-push-to-remote :which-key "magit-push")
+    "gs" '(magit-stage-modified :which-key "magit-stage-modified")
+    "gc" '(magit-commit :which-key "magit-commit")
+
+    "r"  '(:ignore t :which-key "Configuration changes")
+    "rr" '((lambda () (interactive) (load-file "~/.config/emacs/init.el")) :which-key "Reload init.el")
+    "re" '((lambda () (interactive) (find-file "~/.config/emacs/config.org")) :which-key "Open init.el")
+
+    "oy" '(org-store-link :which-key "org-store-link")
+
+    "e"  '(eshell :which-key "Eshell")
+    "v"  '(vterm :which-key "vterm")
+    "x"  '(counsel-M-x :which-key "M-x")
+    "/"  '(evilnc-comment-or-uncomment-lines :which-key "Un/Comment lines")
+   )
+
+  ;; Org-mode specific bindings
+  (general-define-key
+   :states '(normal visual emacs)
+   :keymaps '(org-mode-map)
+   :prefix "SPC"
+   "of" '(org-open-at-point :which-key "org-open-at-point")
+   "oi" '(org-insert-link :which-key "org-insert-link")
+   )
+
+  ;; Rust specific bindings
+  (general-define-key
+   :states '(normal visual emacs)
+   :keymaps '(rustic-mode-map)
+   :prefix "SPC"
+   "cr" '(rustic-cargo-run :which-key "cargo run")
+   "cb" '(rustic-cargo-build-arguments '("--release") :which-key "cargo build --release")
+   "cc" '(rustic-cargo-build :which-key "cargo build")
+   "cd" '(rustic-cargo-build-doc :which-key "cargo doc")
+   "ch" '(lsp-rust-analyzer-inlay-hints-mode :which-key "toggle-inlay-hints") 
+   )
+)
