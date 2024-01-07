977126147b2ed17bff7fb39fb7df43de837a3f10
= <=>
2024-01-07 14:10:52 -0800

    Set font based on system-type

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -61,7 +61,11 @@
 
 ;; Basic configuration tweaks
 ;; Set font
-(set-face-attribute 'default nil :font "Comic Code Ligatures" :height 110)
+(when (eq system-type 'gnu/linux)
+  (setq default-text-properties '(line-spacing 0.25 line-height 1.25))
+  (set-face-attribute 'default nil :font "Comic Code Ligatures" :height 110))
+(when (eq system-type 'darwin)
+  (set-face-attribute 'default nil :font "Comic Code Ligatures" :height 130))
 ;; Disable menu bar
 (menu-bar-mode -1)
 ;; Disable tool bar
@@ -411,7 +415,7 @@
 
   ;; Ensure that pcomplete does not write to the buffer
   ;; and behaves as a pure `completion-at-point-function'.
-  (advice-add 'pcomplete-completions-at-point :around #'cape-wrap-purify)k)
+  (advice-add 'pcomplete-completions-at-point :around #'cape-wrap-purify))
 
 ;; Java integration
 (use-package lsp-java)
