245f22a58a018f0035950739f438434919ff1ccb
= <=>
2024-01-07 08:02:06 -0800

    Garbage collector tweaks on startup

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,3 +1,11 @@
+;; Minimize garbage collection during startup
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; Lower threshold back to 8 MiB (default is 800kB)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (expt 2 23))))
+
 (defvar elpaca-installer-version 0.6)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
