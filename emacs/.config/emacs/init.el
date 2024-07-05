07e31b359623c201e402b6e323e8980541971eed
Caleb Leinz <cleinz+github@pm.me>
2024-07-04 23:36:12 -0700

    emacs: Remove unused packages

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -170,17 +170,9 @@
   (add-hook 'elpaca-after-init-hook #'dashboard-initialize)
   (dashboard-setup-startup-hook))
 
-;; Theme
-(use-package gruber-darker-theme)
-
 (use-package ef-themes
   :config
   (load-theme 'ef-autumn t))
-
-(use-package doom-themes
-  :ensure t)
-
-(use-package spacious-padding)
 
 (use-package doom-modeline
   :ensure t
@@ -488,19 +480,6 @@
   (dirvish-peek-mode)
   (dirvish-override-dired-mode)
   (setq delete-by-moving-to-trash t))
-
-(use-package fzf
-  :config
-  (setq fzf/args "-x --color bw --print-query --margin=1,0 --no-hscroll"
-        fzf/executable "fzf"
-        fzf/git-grep-args "-i --line-number %s"
-        ;; command used for `fzf-grep-*` functions
-        ;; example usage for ripgrep:
-        ;; fzf/grep-command "rg --no-heading -nH"
-        fzf/grep-command "grep -nrH"
-        ;; If nil, the fzf buffer will appear at the top of the window
-        fzf/position-bottom t
-        fzf/window-height 15))
 
 (use-package seq
   :ensure t)			      
