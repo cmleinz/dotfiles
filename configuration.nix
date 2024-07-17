351ee8832ae44079f6ca240c62481d964387a64a
Caleb Leinz <cleinz+github@pm.me>
2024-07-17 05:23:47 -0700

    nixos: Add printer configuration

--- configuration.nix
+++ configuration.nix
@@ -39,6 +39,12 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
+
   services.blueman.enable = true;
 
   # Enable sound with pipewire.
