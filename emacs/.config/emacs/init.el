994fc6a9a378f23fda54aa6e0c0e967d67919f7e
Caleb Leinz <cleinz+github@pm.me>
2024-08-17 00:40:32 -0700

    emacs: Replace corfu with company due to segfault issue

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -524,41 +524,30 @@
     (add-hook 'after-save-hook 'magit-after-save-refresh-status t))
   (setq magit-show-long-lines-warning nil))
 
-;; Code completion
-(use-package corfu
+(use-package company
   :ensure t
-  ;; Optional customizations
   :custom
-  (corfu-cycle t)                 ; Allows cycling through candidates
-  (corfu-auto t)                  ; Enable auto completion
-  (corfu-auto-prefix 2)
-  (corfu-auto-delay 0.2)
-  (corfu-popupinfo-delay '(0.3 . 0.2))
-  (corfu-preview-current 'insert) ; insert previewed candidate
-  (corfu-preselect 'prompt)
-  (corfu-on-exact-match nil)      ; Don't auto expand tempel snippets
-  ;; Optionally use TAB for cycling, default is `corfu-complete'.
-  :bind (:map corfu-map
-	      ("C-n"        . corfu-next)
-	      ("C-p"      . corfu-previous)
-	      ("RET"        . corfu-insert))
-  :init
-  (global-corfu-mode)
-  (corfu-history-mode)
-  (corfu-popupinfo-mode) ; Popup completion info
-  :config
-  (add-hook 'eat-mode-hook
-            (lambda () (setq-local corfu-quit-at-boundary t
-                                   corfu-quit-no-match t
-                                   corfu-auto nil)
-	      (corfu-mode))))
+  (company-idle-delay 0.1) ;; how long to wait until popup
+  ;; (company-begin-commands nil) ;; uncomment to disable popup
+  :hook (prog-mode . company-mode)
+  :bind
+  (:map company-active-map
+        ("<tab>" . company-complete-selection)
+        ("C-n" . company-select-next)
+        ("C-p" . company-select-previous)
+        ("M-<" . company-select-first)
+        ("M->" . company-select-last))
+  )
 
-;; Add symbols to corfu code completions
 (use-package kind-icon
   :ensure t
-  :after corfu
+  :after company
   :config
-  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
+  (let* ((kind-func (lambda (cand) (company-call-backend 'kind cand)))
+         (formatter (kind-icon-margin-formatter `((company-kind . ,kind-func)))))
+    (defun my-company-kind-icon-margin (cand _selected)
+      (funcall formatter cand))
+    (setq company-format-margin-function #'my-company-kind-icon-margin)))
 
 (use-package cape
   :ensure t
