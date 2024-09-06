016128732adee077183001d69eb87395b9b353e0
Caleb Leinz <caleb.leinz@atlasspace.com>
2024-09-06 14:34:14 -0700

    emacs: Add --follow-tags to magit push transient

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -212,7 +212,7 @@
   ;; Configure a custom style dispatcher (see the Consult wiki)
   ;; (setq orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch)
   ;;       orderless-component-separator #'orderless-escapable-split-on-space)
-  (setq completion-styles '(basic orderless)
+  (setq completion-styles '(orderless basic)
         completion-category-defaults nil
         completion-category-overrides '((file (styles partial-completion)))
 	read-buffer-completion-ignore-case t))
